5a24e8498c4f606b25f0a1f8e0e35a4ab298cc35
liutiantian0210 <229804671@qq.com>
2020-04-22 11:59:31 +0800

    测试

---
+++ src/main/java/cn/jiyun/pojo/user.java
@@ -0,0 +1,13 @@
+package cn.jiyun.pojo;
+
+public class user {
+    private String username;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
